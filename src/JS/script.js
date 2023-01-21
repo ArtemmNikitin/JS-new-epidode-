cb8821276a6ef8904ff49c2cbea697e670d8c31a
Артем Никитин <artem-ahead@yandex.ru>
2023-01-21 15:31:16 +0500

    first work

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,51 +1,24 @@
-console.clear();
+"usestrict";
 
-const slider = () => {
-  const slides  = Array.from(document.querySelectorAll('.slider__slide'));
-  const nav     = Array.from(document.querySelectorAll('.slider__nav-btn'));
-  const tabs    = Array.from(document.querySelectorAll('.slider__tab'));
-  
-  let activeSlide = 0;
+const numberOfFilms = + prompt("Сколько фильмов вы уже посмотрели?");
 
-  slides[activeSlide].classList.add('slider__slide_active');
-  tabs[activeSlide].classList.add('slider__nav-btn_active');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
 
-  nav.forEach(item => {
-    item.addEventListener('click', e => {
-      slides[activeSlide].classList.remove('slider__slide_active');
-      tabs[activeSlide].classList.remove('slider__nav-btn_active');
-
-      if(e.target.classList.contains('slider__nav_prev')){
-        if (activeSlide === 0) {
-          activeSlide = slides.length - 1
-        } else{
-          activeSlide--
-        }
-      } else {
-        if (activeSlide === slides.length - 1) {
-          activeSlide = 0
-        } else{
-          activeSlide++
-        }
-      }
-      slides[activeSlide].classList.add('slider__slide_active');
-      tabs[activeSlide].classList.add('slider__nav-btn_active');
-    })
-  });
-  
-  
-  tabs.forEach((item, i) => {
-    item.addEventListener('click', () => {
-      slides[activeSlide].classList.remove('slider__slide_active');
-      tabs[activeSlide].classList.remove('slider__nav-btn_active');
-
-      activeSlide = i;
-
-      slides[activeSlide].classList.add('slider__slide_active');
-      tabs[activeSlide].classList.add('slider__nav-btn_active');
-    });
-  });
-}
+const a = prompt('Один из последних просмотренных фильмов',""),
+		b = +prompt('На сколько оцениваете его?', ""),
+		c = prompt('Один из последних просмотренных фильмов',""),
+		d = +prompt('На сколько оцениваете его?', "");
 
 
-slider()
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
